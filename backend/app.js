bd825e0161661f1caea1068c4906ad7119dbfdf2
kshitijk83 <kshitijk83@gmail.com>
2020-03-20 17:32:42 +0530

    minor

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Credentials", true);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:8080';
+// export const BASE_URL = 'http://127.0.0.1:8080';
+export const BASE_URL = 'https://recapchatest.herokuapp.com/';
 export const CHECK_ROUTE = BASE_URL+'/check';
 export const SIGNUP_ROUTE = BASE_URL+'/auth/signup';
